0afcb8f76789756ddce80b2890ec2a83c3c459a3
Anelkad <an_kadyova@kbtu.kz>
2024-01-12 12:25:23 +0600

    layout: search input

---
+++ app/src/main/java/com/example/contacts/SearchInput.kt
@@ -0,0 +1,47 @@
+package com.example.contacts
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.core.widget.addTextChangedListener
+import com.example.contacts.databinding.InputSearchBinding
+
+class SearchInput @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val binding = InputSearchBinding.inflate(LayoutInflater.from(context), this)
+    private var searchListener: SearchListener? = null
+
+    init {
+        binding.etSearch.addTextChangedListener { content ->
+            searchListener?.searchQuery(content.toString())
+        }
+        binding.ivClear.setOnClickListener {
+            binding.etSearch.text?.clear()
+            binding.etSearch.clearFocus()
+        }
+    }
+
+    fun addSearchListener(listener: SearchListener) {
+        searchListener = listener
+    }
+
+    fun getText(): String {
+        return if (binding.etSearch.text.isNullOrEmpty())
+            ""
+        else
+            binding.etSearch.text.toString()
+    }
+
+    fun setText(text: String) {
+        binding.etSearch.setText(text)
+    }
+}
+
+interface SearchListener {
+    fun searchQuery(query: String?)
+}
